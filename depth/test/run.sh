1ea29575a659cfe1fab7ff4e605fd8ced567046d
chapmanb <chapmanb@50mail.com>
2016-10-11 11:05:10 -0400

    Update test cases: fix previous and add empty
    New empty test demonstrates problems with `-1  0` output at the start of
    chromosomes (chapmanb/bcbio-nextgen#1583)

--- depth/test/run.sh
+++ depth/test/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+set -eu -o pipefail
 
-goleft depth --windowsize 10 --q 1 --mincov 4 --reference hg19.fa --processes 1 --stats --bed Test1-coverage.depth-tocalculate-windows.bed --prefix out Test1-sort.bam
+# Standard run
+goleft depth --windowsize 10 --q 1 --mincov 4 --reference hg19.fa --processes 1 --stats --bed windows.bed --prefix out t.bam
+
+
+# Empty BAM file, simulating empty chromosomes
+goleft depth --windowsize 10 --q 1 --mincov 4 --reference hg19.fa --processes 1 --stats --prefix out-empty t-empty.bam
